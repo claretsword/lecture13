5b96c9473d0c9a9d898b6668902c9d7b6795b93d
seunghwan <wkdwnsdud0897@naver.com>
2024-05-29 15:39:57 +0900

    과재즁

--- lecture13/lecture13.cpp
+++ lecture13/lecture13.cpp
@@ -5,11 +5,52 @@
 #include <cmath>
 
 
+// 전역 변수로 playerY 선언
+float playerY = 0.0f; // 플레이어의 y축 위치 초기값
+
 // 에러 콜백 함수
 void errorCallback(int error, const char* description)
 {
     std::cerr << "GLFW Error: " << description << std::endl;
 }
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+    
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+    else if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+    {
+        playerY += 0.2f;
+    }
+}
+
+
+int render()
+{
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex2f(10.0f, -0.5f);
+    glVertex2f(-10.0f, -0.5f);
+    glVertex2f(10.0f, -10.0f);
+    glVertex2f(-10.0f, -10.0f);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glVertex2f(-0.1f, -0.5f+ playerY);
+    glVertex2f(-0.1f, -0.4f+ playerY);
+    glVertex2f(0.1f, -0.4f+ playerY);
+    glVertex2f(0.1f, -0.5f+ playerY);
+    glEnd();
+
+    return 0;
+}
+
 
 
 int main(void)
@@ -21,6 +62,7 @@ int main(void)
     // 윈도우 생성
     GLFWwindow* window;
     window = glfwCreateWindow(1280, 768, "MuSoeunEngine", NULL, NULL);
+    
     if (!window)
     {
         glfwTerminate();
@@ -32,17 +74,21 @@ int main(void)
 
     // GLFW 콜백 함수 설정
     glfwSetErrorCallback(errorCallback);
-   
+    glfwSetKeyCallback(window, keyCallback);
 
     // 렌더링 루프
     while (!glfwWindowShouldClose(window))
     {
         // 이벤트 처리
         glfwPollEvents();
-
+        
         // 화면 지우기
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        //랜더링 함수 호출
+        render();
+       
 
         // 화면 전환
         glfwSwapBuffers(window);
